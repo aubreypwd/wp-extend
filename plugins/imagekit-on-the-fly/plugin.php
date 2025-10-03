33e4fe70927c663a11620e32f8921049dc64b04c
Aubrey Portwood <code@aubreypwd.com>
2025-10-03 14:45:32 -0600

    Add preconnect and dns-fetch.

--- plugins/imagekit-on-the-fly/plugin.php
+++ plugins/imagekit-on-the-fly/plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name:       Imagekit on-the-fly
+ * Plugin Name:       Imagekit: On-the-fly
  * Plugin URI:        https://aubreypwd.com
- * Description:       This switches out all images on the frontend with their Imagekit CDN counterparts.
+ * Description:       This switches out all images on the frontend (on-the-fly) with their Imagekit counterparts transformed.
  * Version:           1.0.0
  * Author:            Aubrey Portwood
  * Author URI:        https://aubreypwd.com
@@ -27,7 +27,7 @@ if ( wp_doing_ajax() || wp_doing_cron() ) {
 
 @define( 'AUBREYPWD_IMAGEKIT_ON_THE_FLY_USERNAME', '' );
 @define( 'AUBREYPWD_IMAGEKIT_ON_THE_FLY_IMAGE_TR', 'f-webp,q-71,md-false,cp-false' ); // See https://imagekit.io/docs/image-transformation.
-@define( 'AUBREYPWD_IMAGEKIT_ON_THE_FLY_UPTIME_IMAGE', plugins_url( 'pixel.gif', __FILE__ ) ); // Override if you want to use a pixel somewhere else.
+@define( 'AUBREYPWD_IMAGEKIT_ON_THE_FLY_UPTIME_IMAGE', plugins_url( 'uptime-pixel.gif', __FILE__ ) ); // Override if you want to use a pixel somewhere else.
 
 if (
 	empty( AUBREYPWD_IMAGEKIT_ON_THE_FLY_USERNAME )
@@ -39,7 +39,7 @@ if (
 
 // Whether or not do do images and/or video, define these or use filter below.
 @define( 'AUBREYPWD_IMAGEKIT_ON_THE_FLY_DO_IMAGES', true );
-@define( 'AUBREYPWD_IMAGEKIT_ON_THE_FLY_DO_VIDEO', true );
+@define( 'AUBREYPWD_IMAGEKIT_ON_THE_FLY_DO_VIDEO', false ); // Off by default, just because videos can iffy.
 
 /**
  * Convert content to use imagekit.
@@ -51,89 +51,99 @@ function convert_content( string $content ) {
 	// Get the current uploads URI (including domain) so we can use it to switch out images/videos in the content...
 	$uploads_uri = trim( str_replace( [ 'http://', 'https://' ], '', wp_get_upload_dir()['baseurl'] ), '/' );
 
-	if (
-		apply_filters( 'aubreypwd/imagekit_on_the_fly/do_images', AUBREYPWD_IMAGEKIT_ON_THE_FLY_DO_IMAGES )
-	) {
+	// Convert images...
+	if ( apply_filters( 'aubreypwd/imagekit_on_the_fly/do_images', AUBREYPWD_IMAGEKIT_ON_THE_FLY_DO_IMAGES ) ) {
 
+		// Image replacement: add transformations defined above.
+		$content = preg_replace(
+
+			/**
+			 * Filter preg_replace pattern (images).
+			 *
+			 * @param string $pattern The pattern.
+			 */
+			apply_filters(
+				'aubreypwd/imagekit_on_the_fly/preg_replace/images/pattern',
+				sprintf(
+					'#https?://%s/([^\s"\']+?\.(jpe?g|png|bmp|webp))#i',
+					$uploads_uri
+				)
+			),
+
+			/**
+			 * Filter preg_replace replacement pattern (images).
+			 *
+			 * @param string $replacement_pattern The replacement pattern.
+			 */
+			apply_filters(
+				'aubreypwd/imagekit_on_the_fly/preg_replace/images/replacement_pattern',
+				sprintf(
+					'https://ik.imagekit.io/%s/tr:%s/$1',
+					AUBREYPWD_IMAGEKIT_ON_THE_FLY_USERNAME,
+
+					/**
+					 * Filter transformations.
+					 *
+					 * @param string $transformations See https://imagekit.io/docs/image-transformation.
+					 */
+					apply_filters( 'aubreypwd/imagekit_on_the_fly/tr', AUBREYPWD_IMAGEKIT_ON_THE_FLY_IMAGE_TR )
+				)
+			),
+			$content
+		);
 	}
 
-	// Image replacement: add transformations defined above.
-	$content = preg_replace(
+	// Convert video.
+	if ( apply_filters( 'aubreypwd/imagekit_on_the_fly/do_video', AUBREYPWD_IMAGEKIT_ON_THE_FLY_DO_VIDEO ) ) {
 
-		/**
-		 * Filter preg_replace pattern (images).
-		 *
-		 * @param string $pattern The pattern.
-		 */
-		apply_filters(
-			'aubreypwd/imagekit_on_the_fly/preg_replace/images/pattern',
-			sprintf(
-				'#https?://%s/([^\s"\']+?\.(jpe?g|png|bmp|webp))#i',
-				$uploads_uri
-			)
-		),
+		// Video replacement, no transformations, but hosted on imagekit.
+		$content = preg_replace(
 
-		/**
-		 * Filter preg_replace replacement pattern (images).
-		 *
-		 * @param string $replacement_pattern The replacement pattern.
-		 */
-		apply_filters(
-			'aubreypwd/imagekit_on_the_fly/preg_replace/images/replacement_pattern',
-			sprintf(
-				'https://ik.imagekit.io/%s/tr:%s/$1',
-				AUBREYPWD_IMAGEKIT_ON_THE_FLY_USERNAME,
+			/**
+			 * Filter preg_replace pattern (video).
+			 *
+			 * @param string $pattern The pattern.
+			 */
+			apply_filters(
+				'aubreypwd/imagekit_on_the_fly/preg_replace/video/pattern',
+				sprintf(
+					'#https?://%s/([^\s"\']+?\.(mp4|webm|mov))#i',
+					$uploads_uri
+				)
+			),
 
-				/**
-				 * Filter transformations.
-				 *
-				 * @param string $transformations See https://imagekit.io/docs/image-transformation.
-				 */
-				apply_filters( 'aubreypwd/imagekit_on_the_fly/tr', AUBREYPWD_IMAGEKIT_ON_THE_FLY_IMAGE_TR )
-			)
-		),
-		$content
-	);
-
-	// Video replacement, no transformations, but hosted on imagekit.
-	$content = preg_replace(
-
-		/**
-		 * Filter preg_replace pattern (video).
-		 *
-		 * @param string $pattern The pattern.
-		 */
-		apply_filters(
-			'aubreypwd/imagekit_on_the_fly/preg_replace/video/pattern',
-			sprintf(
-				'#https?://%s/([^\s"\']+?\.(mp4|webm|mov))#i',
-				$uploads_uri
-			)
-		),
-
-		/**
-		 * Filter preg_replace replacement pattern (video).
-		 *
-		 * @param string $replacement_pattern The replacement pattern.
-		 */
-		apply_filters(
-			'aubreypwd/imagekit_on_the_fly/preg_replace/video/replacement_pattern',
-			sprintf(
-				'https://ik.imagekit.io/%s/$1',
-				AUBREYPWD_IMAGEKIT_ON_THE_FLY_USERNAME
-			)
-		),
-		$content
-	);
+			/**
+			 * Filter preg_replace replacement pattern (video).
+			 *
+			 * @param string $replacement_pattern The replacement pattern.
+			 */
+			apply_filters(
+				'aubreypwd/imagekit_on_the_fly/preg_replace/video/replacement_pattern',
+				sprintf(
+					'https://ik.imagekit.io/%s/$1',
+					AUBREYPWD_IMAGEKIT_ON_THE_FLY_USERNAME
+				)
+			),
+			$content
+		);
+	}
 
 	return $content;
 }
+
+// Add preconnect rule for imagekit.io.
+add_action( 'wp_head', function() {
+	?>
+	<link rel="preconnect" href="https://ik.imagekit.io" crossorigin>
+	<link rel="dns-prefetch" href="//ik.imagekit.io">
+	<?php
+}, PHP_INT_MIN );
 
 // When all plugins have loaded...
 add_action( 'plugins_loaded', function() {
 
 	// Replace images/video links with imagekit ones.
-	add_action(  'template_redirect', function() {
+	add_action( 'template_redirect', function() {
 
 		// Buffer the entire content of the page.
 		ob_start( function( $buffer ) {
@@ -159,25 +169,23 @@ add_action( 'plugins_loaded', function() {
 			// We don't know if it failed or not, let's test.
 			} else {
 
-				// Check the pixel on the server, it should translate to a 200 OK if imagekit is up.
-				$headers = @get_headers(
-
-					/**
-					 * Filter imagekit uptime image URL.
-					 *
-					 * @param string $uri The URI where your pixel should be on imagekit.
-					 */
-					apply_filters(
-						'aubreypwd/imagekit_on_the_fly/uptime_image',
-						AUBREYPWD_IMAGEKIT_ON_THE_FLY_UPTIME_IMAGE
+				// See if imagekit generates an image for our pixel, if so we can trust imagekit.
+				$response_code = wp_remote_retrieve_response_code(
+					wp_remote_get(
+						AUBREYPWD_IMAGEKIT_ON_THE_FLY_UPTIME_IMAGE,
+						[
+							'timeout'   => 5,
+							'method'    => 'HEAD',
+							'sslverify' => false,
+						]
 					)
 				);
 
 				// We got a 200 OK from imagekit.
-				if ( strpos( ( $headers[0] ?? '' ), '200' ) ) {
+				if ( 200 === $response_code ) {
 
-					// Remember this success for an hour (we'll try again after an hour).
-					set_transient( $transient, 'succeeded', HOUR_IN_SECONDS );
+					// Trust it for today.
+					set_transient( $transient, 'succeeded', DAY_IN_SECONDS );
 
 					// Use converted content since we can trust the source.
 					return convert_content( $buffer );
